f163692f67109bebd0918356b38e78bd0e3f041d
Jens Schulze <jenschude@gmail.com>
2020-10-16 13:25:02 +0200

    cleanup

--- commercetools.SDK/commercetools.Api/DependencyInjectionSetup.cs
+++ commercetools.SDK/commercetools.Api/DependencyInjectionSetup.cs
@@ -5,11 +5,9 @@ using commercetools.Api.Serialization.Mappers.CustomFieldMappers;
 using commercetools.Api.Serialization.MapperTypeRetrievers;
 using commercetools.Base.Serialization.MapperTypeRetrievers;
 using Microsoft.Extensions.DependencyInjection;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using commercetools.Base.Client;
-using commercetools.Base.Client.Tokens;
 using commercetools.Base.Registration;
 using commercetools.Base.Serialization;
 using Microsoft.Extensions.Configuration;
@@ -28,13 +26,6 @@ namespace commercetools.Api
             return services.UseCommercetoolsApi(configuration, clients).Single().Value;
         }
         
-        /// <summary>
-        /// Adds concrete implementations necessary for running of the application to the service collection for a single client.
-        /// </summary>
-        /// <param name="services">The service collection.</param>
-        /// <param name="configuration">The configuration.</param>
-        /// <param name="clientName">The name of the client.</param>
-        /// <param name="tokenFlow">The token flow.</param>
         public static IDictionary<string, IHttpClientBuilder> UseCommercetoolsApi(this IServiceCollection services, IConfiguration configuration, IList<string> clients)
         {
             services.UseCommercetoolsApiSerialization();
