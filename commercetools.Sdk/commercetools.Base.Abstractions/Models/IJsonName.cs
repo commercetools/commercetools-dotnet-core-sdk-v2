a4c9fb9b402b6405122c9f9349e6e1cfd302353a
Michele George <michele.george@commercetools.de>
2020-11-18 16:52:09 +0100

    Adding IJsonName interface to support EnumInterface Serialization

---
+++ commercetools.Sdk/commercetools.Base.Abstractions/Models/IJsonName.cs
@@ -0,0 +1,7 @@
+namespace commercetools.Base.Models
+{
+    public interface IJsonName
+    {
+        string JsonName { get; }
+    }
+}
--- commercetools.Sdk/commercetools.Base.Serialization/JsonConverters/EnumInterfaceConverter.cs
+++ commercetools.Sdk/commercetools.Base.Serialization/JsonConverters/EnumInterfaceConverter.cs
@@ -3,6 +3,7 @@ using System.Reflection;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using commercetools.Base.CustomAttributes;
+using commercetools.Base.Models;
 
 namespace commercetools.Base.Serialization.JsonConverters
 {
@@ -28,11 +29,13 @@ namespace commercetools.Base.Serialization.JsonConverters
             var attrDelegate = attr.Creator();
             return (T)attrDelegate.DynamicInvoke(reader.GetString());
         }
-
-        /// <inheritdoc/>
+        
         public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
         {
-            throw new NotImplementedException();
+            if (value is IJsonName withJsonName)
+            {
+                JsonSerializer.Serialize(writer, withJsonName.JsonName, options);
+            }
         }
     }
 }
