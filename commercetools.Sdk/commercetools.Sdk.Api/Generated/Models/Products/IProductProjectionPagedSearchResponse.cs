6310b2a53341e56fe06dbaa2a005a1e810f9d68e
Auto Mation <automation@commercetools.com>
2020-11-25 10:41:28 +0000

    TASK: Updating SDK

--- commercetools.Sdk/commercetools.Sdk.Api/Generated/Models/Products/IProductProjectionPagedSearchResponse.cs
+++ commercetools.Sdk/commercetools.Sdk.Api/Generated/Models/Products/IProductProjectionPagedSearchResponse.cs
@@ -11,6 +11,8 @@ namespace commercetools.Api.Models.Products
     [DeserializeAs(typeof(commercetools.Api.Models.Products.ProductProjectionPagedSearchResponse))]
     public partial interface IProductProjectionPagedSearchResponse 
     {
+        long Limit { get; set;}
+        
         long Count { get; set;}
         
         long? Total { get; set;}
