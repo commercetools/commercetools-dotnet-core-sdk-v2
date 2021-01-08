4efb5ed551b6a5deb104ffb1bd1a98ebe8e08c9f
Auto Mation <automation@commercetools.com>
2021-01-08 20:22:46 +0000

    TASK: Updating SDK

--- commercetools.Sdk/commercetools.Sdk.Api/Generated/Models/Common/ReferenceTypeId.cs
+++ commercetools.Sdk/commercetools.Sdk.Api/Generated/Models/Common/ReferenceTypeId.cs
@@ -21,14 +21,32 @@ namespace commercetools.Api.Models.Common
        [Description("customer")]
        Customer,
        
+       [Description("customer-email-token")]
+       CustomerEmailToken,
+       
        [Description("customer-group")]
        CustomerGroup,
+       
+       [Description("customer-password-token")]
+       CustomerPasswordToken,
        
        [Description("discount-code")]
        DiscountCode,
        
+       [Description("extension")]
+       Extension,
+       
+       [Description("inventory-entry")]
+       InventoryEntry,
+       
        [Description("key-value-document")]
        KeyValueDocument,
+       
+       [Description("order")]
+       Order,
+       
+       [Description("order-edit")]
+       OrderEdit,
        
        [Description("payment")]
        Payment,
@@ -36,29 +54,29 @@ namespace commercetools.Api.Models.Common
        [Description("product")]
        Product,
        
-       [Description("product-type")]
-       ProductType,
-       
        [Description("product-discount")]
        ProductDiscount,
        
-       [Description("order")]
-       Order,
+       [Description("product-type")]
+       ProductType,
        
        [Description("review")]
        Review,
        
-       [Description("shopping-list")]
-       ShoppingList,
-       
        [Description("shipping-method")]
        ShippingMethod,
+       
+       [Description("shopping-list")]
+       ShoppingList,
        
        [Description("state")]
        State,
        
        [Description("store")]
        Store,
+       
+       [Description("subscription")]
+       Subscription,
        
        [Description("tax-category")]
        TaxCategory,
@@ -67,13 +85,7 @@ namespace commercetools.Api.Models.Common
        Type,
        
        [Description("zone")]
-       Zone,
-       
-       [Description("inventory-entry")]
-       InventoryEntry,
-       
-       [Description("order-edit")]
-       OrderEdit
+       Zone
    }
    public class ReferenceTypeIdWrapper : IReferenceTypeId
    {
@@ -98,14 +110,32 @@ namespace commercetools.Api.Models.Common
         public static IReferenceTypeId Customer = new ReferenceTypeIdWrapper
          {Value = ReferenceTypeId.Customer, JsonName = "customer"}; 
        
+        public static IReferenceTypeId CustomerEmailToken = new ReferenceTypeIdWrapper
+         {Value = ReferenceTypeId.CustomerEmailToken, JsonName = "customer-email-token"}; 
+       
         public static IReferenceTypeId CustomerGroup = new ReferenceTypeIdWrapper
          {Value = ReferenceTypeId.CustomerGroup, JsonName = "customer-group"}; 
+       
+        public static IReferenceTypeId CustomerPasswordToken = new ReferenceTypeIdWrapper
+         {Value = ReferenceTypeId.CustomerPasswordToken, JsonName = "customer-password-token"}; 
        
         public static IReferenceTypeId DiscountCode = new ReferenceTypeIdWrapper
          {Value = ReferenceTypeId.DiscountCode, JsonName = "discount-code"}; 
        
+        public static IReferenceTypeId Extension = new ReferenceTypeIdWrapper
+         {Value = ReferenceTypeId.Extension, JsonName = "extension"}; 
+       
+        public static IReferenceTypeId InventoryEntry = new ReferenceTypeIdWrapper
+         {Value = ReferenceTypeId.InventoryEntry, JsonName = "inventory-entry"}; 
+       
         public static IReferenceTypeId KeyValueDocument = new ReferenceTypeIdWrapper
          {Value = ReferenceTypeId.KeyValueDocument, JsonName = "key-value-document"}; 
+       
+        public static IReferenceTypeId Order = new ReferenceTypeIdWrapper
+         {Value = ReferenceTypeId.Order, JsonName = "order"}; 
+       
+        public static IReferenceTypeId OrderEdit = new ReferenceTypeIdWrapper
+         {Value = ReferenceTypeId.OrderEdit, JsonName = "order-edit"}; 
        
         public static IReferenceTypeId Payment = new ReferenceTypeIdWrapper
          {Value = ReferenceTypeId.Payment, JsonName = "payment"}; 
@@ -113,29 +143,29 @@ namespace commercetools.Api.Models.Common
         public static IReferenceTypeId Product = new ReferenceTypeIdWrapper
          {Value = ReferenceTypeId.Product, JsonName = "product"}; 
        
-        public static IReferenceTypeId ProductType = new ReferenceTypeIdWrapper
-         {Value = ReferenceTypeId.ProductType, JsonName = "product-type"}; 
-       
         public static IReferenceTypeId ProductDiscount = new ReferenceTypeIdWrapper
          {Value = ReferenceTypeId.ProductDiscount, JsonName = "product-discount"}; 
        
-        public static IReferenceTypeId Order = new ReferenceTypeIdWrapper
-         {Value = ReferenceTypeId.Order, JsonName = "order"}; 
+        public static IReferenceTypeId ProductType = new ReferenceTypeIdWrapper
+         {Value = ReferenceTypeId.ProductType, JsonName = "product-type"}; 
        
         public static IReferenceTypeId Review = new ReferenceTypeIdWrapper
          {Value = ReferenceTypeId.Review, JsonName = "review"}; 
        
-        public static IReferenceTypeId ShoppingList = new ReferenceTypeIdWrapper
-         {Value = ReferenceTypeId.ShoppingList, JsonName = "shopping-list"}; 
-       
         public static IReferenceTypeId ShippingMethod = new ReferenceTypeIdWrapper
          {Value = ReferenceTypeId.ShippingMethod, JsonName = "shipping-method"}; 
+       
+        public static IReferenceTypeId ShoppingList = new ReferenceTypeIdWrapper
+         {Value = ReferenceTypeId.ShoppingList, JsonName = "shopping-list"}; 
        
         public static IReferenceTypeId State = new ReferenceTypeIdWrapper
          {Value = ReferenceTypeId.State, JsonName = "state"}; 
        
         public static IReferenceTypeId Store = new ReferenceTypeIdWrapper
          {Value = ReferenceTypeId.Store, JsonName = "store"}; 
+       
+        public static IReferenceTypeId Subscription = new ReferenceTypeIdWrapper
+         {Value = ReferenceTypeId.Subscription, JsonName = "subscription"}; 
        
         public static IReferenceTypeId TaxCategory = new ReferenceTypeIdWrapper
          {Value = ReferenceTypeId.TaxCategory, JsonName = "tax-category"}; 
@@ -145,12 +175,6 @@ namespace commercetools.Api.Models.Common
        
         public static IReferenceTypeId Zone = new ReferenceTypeIdWrapper
          {Value = ReferenceTypeId.Zone, JsonName = "zone"}; 
-       
-        public static IReferenceTypeId InventoryEntry = new ReferenceTypeIdWrapper
-         {Value = ReferenceTypeId.InventoryEntry, JsonName = "inventory-entry"}; 
-       
-        public static IReferenceTypeId OrderEdit = new ReferenceTypeIdWrapper
-         {Value = ReferenceTypeId.OrderEdit, JsonName = "order-edit"}; 
        
         ReferenceTypeId? Value { get; }
         
@@ -163,24 +187,28 @@ namespace commercetools.Api.Models.Common
                 Category ,
                 Channel ,
                 Customer ,
+                CustomerEmailToken ,
                 CustomerGroup ,
+                CustomerPasswordToken ,
                 DiscountCode ,
+                Extension ,
+                InventoryEntry ,
                 KeyValueDocument ,
+                Order ,
+                OrderEdit ,
                 Payment ,
                 Product ,
-                ProductType ,
                 ProductDiscount ,
-                Order ,
+                ProductType ,
                 Review ,
-                ShoppingList ,
                 ShippingMethod ,
+                ShoppingList ,
                 State ,
                 Store ,
+                Subscription ,
                 TaxCategory ,
                 Type ,
-                Zone ,
-                InventoryEntry ,
-                OrderEdit 
+                Zone 
            };
         }
         static IReferenceTypeId FindEnum(string value)
