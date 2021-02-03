bd25e92149dab4cd3ad67552b3ebee3d52b408ac
Auto Mation <automation@commercetools.com>
2021-02-03 07:45:24 +0000

    TASK: Updating SDK

--- commercetools.Sdk/commercetools.Sdk.Api/Generated/Client/RequestBuilders/CustomObjects/ByProjectKeyCustomObjectsByContainerGet.cs
+++ commercetools.Sdk/commercetools.Sdk.Api/Generated/Client/RequestBuilders/CustomObjects/ByProjectKeyCustomObjectsByContainerGet.cs
@@ -29,18 +29,26 @@ namespace commercetools.Api.Client.RequestBuilders.CustomObjects
            this.RequestUrl = $"/{ProjectKey}/custom-objects/{Container}";
        }
    
+       public List<string> GetWhere() {
+           return this.GetQueryParam("where");
+       }
+       
        public List<string> GetExpand() {
            return this.GetQueryParam("expand");
        }
    
+       public ByProjectKeyCustomObjectsByContainerGet WithWhere(string where){
+           return this.AddQueryParam("where", where);
+       }
+       
        public ByProjectKeyCustomObjectsByContainerGet WithExpand(string expand){
            return this.AddQueryParam("expand", expand);
        }
 
-       public async Task<commercetools.Api.Models.CustomObjects.CustomObject> ExecuteAsync()
+       public async Task<commercetools.Api.Models.CustomObjects.CustomObjectPagedQueryResponse> ExecuteAsync()
        {
           var requestMessage = Build();
-          return await ApiHttpClient.ExecuteAsync<commercetools.Api.Models.CustomObjects.CustomObject>(requestMessage);
+          return await ApiHttpClient.ExecuteAsync<commercetools.Api.Models.CustomObjects.CustomObjectPagedQueryResponse>(requestMessage);
        }
    }
 }
