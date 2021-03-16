defaec1c3231e18ac6ffb480b84fe878d0b19521
Auto Mation <automation@commercetools.com>
2021-03-16 09:54:35 +0000

    TASK: Updating SDK

--- commercetools.Sdk/commercetools.Sdk.Api/Generated/Models/CartDiscounts/ICartDiscountTarget.cs
+++ commercetools.Sdk/commercetools.Sdk.Api/Generated/Models/CartDiscounts/ICartDiscountTarget.cs
@@ -10,9 +10,9 @@ namespace commercetools.Api.Models.CartDiscounts
     [TypeDiscriminator(nameof(Type))]
     [SubTypeDiscriminator("multiBuyCustomLineItems", typeof(commercetools.Api.Models.CartDiscounts.MultiBuyCustomLineItemsTarget))]
     [SubTypeDiscriminator("multiBuyLineItems", typeof(commercetools.Api.Models.CartDiscounts.MultiBuyLineItemsTarget))]
-    [SubTypeDiscriminator("shipping", typeof(commercetools.Api.Models.CartDiscounts.CartDiscountShippingCostTarget))]
     [SubTypeDiscriminator("customLineItems", typeof(commercetools.Api.Models.CartDiscounts.CartDiscountCustomLineItemsTarget))]
     [SubTypeDiscriminator("lineItems", typeof(commercetools.Api.Models.CartDiscounts.CartDiscountLineItemsTarget))]
+    [SubTypeDiscriminator("shipping", typeof(commercetools.Api.Models.CartDiscounts.CartDiscountShippingCostTarget))]
     public partial interface ICartDiscountTarget 
     {
         string Type { get; set;}
--- commercetools.Sdk/commercetools.Sdk.Api/Generated/Models/ShippingMethods/IShippingRatePriceTier.cs
+++ commercetools.Sdk/commercetools.Sdk.Api/Generated/Models/ShippingMethods/IShippingRatePriceTier.cs
@@ -9,9 +9,9 @@ using commercetools.Base.CustomAttributes;
 namespace commercetools.Api.Models.ShippingMethods
 {
     [TypeDiscriminator(nameof(Type))]
+    [SubTypeDiscriminator("CartScore", typeof(commercetools.Api.Models.ShippingMethods.CartScoreTier))]
     [SubTypeDiscriminator("CartClassification", typeof(commercetools.Api.Models.ShippingMethods.CartClassificationTier))]
     [SubTypeDiscriminator("CartValue", typeof(commercetools.Api.Models.ShippingMethods.CartValueTier))]
-    [SubTypeDiscriminator("CartScore", typeof(commercetools.Api.Models.ShippingMethods.CartScoreTier))]
     public partial interface IShippingRatePriceTier 
     {
         IShippingRateTierType Type { get; set;}
