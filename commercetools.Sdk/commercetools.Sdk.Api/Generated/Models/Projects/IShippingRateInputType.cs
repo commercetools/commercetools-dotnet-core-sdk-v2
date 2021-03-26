12847ee9acf54458144da030dc113fd356841ae3
Auto Mation <automation@commercetools.com>
2021-03-26 12:31:36 +0000

    TASK: Updating SDK

--- commercetools.Sdk/commercetools.Sdk.Api/Generated/Models/Projects/IShippingRateInputType.cs
+++ commercetools.Sdk/commercetools.Sdk.Api/Generated/Models/Projects/IShippingRateInputType.cs
@@ -9,8 +9,8 @@ using commercetools.Base.CustomAttributes;
 namespace commercetools.Api.Models.Projects
 {
     [TypeDiscriminator(nameof(Type))]
-    [SubTypeDiscriminator("CartClassification", typeof(commercetools.Api.Models.Projects.CartClassificationType))]
     [SubTypeDiscriminator("CartScore", typeof(commercetools.Api.Models.Projects.CartScoreType))]
+    [SubTypeDiscriminator("CartClassification", typeof(commercetools.Api.Models.Projects.CartClassificationType))]
     [SubTypeDiscriminator("CartValue", typeof(commercetools.Api.Models.Projects.CartValueType))]
     public partial interface IShippingRateInputType 
     {
