e25513ee8a63c7937d4f0a5e140043e0fa509ffd
Auto Mation <automation@commercetools.com>
2021-05-19 07:28:31 +0000

    TASK: Updating SDK

--- commercetools.Sdk/commercetools.Sdk.Api/Generated/Models/Messages/IOrderStateTransitionMessage.cs
+++ commercetools.Sdk/commercetools.Sdk.Api/Generated/Models/Messages/IOrderStateTransitionMessage.cs
@@ -15,6 +15,8 @@ namespace commercetools.Api.Models.Messages
     {
         IStateReference State { get; set;}
         
+        IStateReference OldState { get; set;}
+        
         bool Force { get; set;}
     }
 }
