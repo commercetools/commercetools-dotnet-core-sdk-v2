5f2feeec8c274fdfe9b846765b8ae02889ad3663
Auto Mation <automation@commercetools.com>
2021-12-10 13:29:16 +0000

    TASK: Updating SDK

---
+++ commercetools.Sdk/commercetools.Sdk.Api/Generated/Models/Me/IMyCartSetLineItemSupplyChannelAction.cs
@@ -0,0 +1,14 @@
+using commercetools.Api.Models.Channels;
+using commercetools.Base.CustomAttributes;
+
+
+namespace commercetools.Api.Models.Me
+{
+    [DeserializeAs(typeof(commercetools.Api.Models.Me.MyCartSetLineItemSupplyChannelAction))]
+    public partial interface IMyCartSetLineItemSupplyChannelAction : IMyCartUpdateAction
+    {
+        string LineItemId { get; set; }
+
+        IChannelResourceIdentifier DistributionChannel { get; set; }
+    }
+}
