0e8a11df2b2e823693b227fa8aab67c4edfb55c6
Jens Schulze <jens.schulze@commercetools.com>
2022-01-28 09:32:32 +0100

    fix mapping of pOBox field
    Closes #129

---
+++ commercetools.Sdk/Tests/commercetools.Api.Serialization.Tests/AddressTests.cs
@@ -0,0 +1,40 @@
+using System.IO;
+using commercetools.Api.Models.Common;
+using Xunit;
+
+namespace commercetools.Api.Serialization.Tests
+{
+    public class AddressTests: IClassFixture<SerializationFixture>
+    {
+        private readonly SerializationFixture _serializationFixture;
+
+        public AddressTests(SerializationFixture serializationFixture)
+        {
+            this._serializationFixture = serializationFixture;
+        }
+
+        [Fact]
+        public void poBoxAddress()
+        {
+            var serializerService = this._serializationFixture.SerializerService;
+            var serialized = "{\"pOBox\": \"12345\"}";
+            var deserialized = serializerService.Deserialize<Address>(serialized);
+            Assert.NotNull(deserialized);
+            Assert.Equal("12345", deserialized.POBox);
+
+            Assert.Equal("{\"pOBox\":\"12345\"}", serializerService.Serialize(deserialized));
+        }
+        [Fact]
+        public void poBoxAddressDraft()
+        {
+            var serializerService = this._serializationFixture.SerializerService;
+            var serialized = "{\"pOBox\": \"12345\"}";
+            var deserialized = serializerService.Deserialize<AddressDraft>(serialized);
+            Assert.NotNull(deserialized);
+            Assert.Equal("12345", deserialized.POBox);
+
+            Assert.Equal("{\"pOBox\":\"12345\"}", serializerService.Serialize(deserialized));
+        }
+
+    }
+}
