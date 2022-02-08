92ea3493d9cbf402c9d6f0e4e6221fa5638f18b1
Jens Schulze <jens.schulze@commercetools.com>
2022-02-08 14:35:12 +0100

    prepare for model renamings

--- commercetools.Sdk/commercetools.Sdk.Api/Models/Subscriptions/IMessageDelivery.cs
+++ commercetools.Sdk/commercetools.Sdk.Api/Models/Subscriptions/IMessageDelivery.cs
@@ -2,7 +2,7 @@ using commercetools.Api.Models.Messages;
 
 namespace commercetools.Api.Models.Subscriptions
 {
-    public partial interface IMessageDelivery : ISubscriptionDelivery
+    public partial interface IMessageDelivery : IDeliveryPayload
     {
         IMessage Message { get; set; }
     }
