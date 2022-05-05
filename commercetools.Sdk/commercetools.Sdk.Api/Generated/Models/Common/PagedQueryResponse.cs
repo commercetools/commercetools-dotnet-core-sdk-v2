b02aaee8ddb37d65027866fac88d4747a0e20c6b
Auto Mation <automation@commercetools.com>
2022-05-05 08:32:39 +0000

    TASK: Updating SDK

--- commercetools.Sdk/commercetools.Sdk.Api/Generated/Models/Common/PagedQueryResponse.cs
+++ commercetools.Sdk/commercetools.Sdk.Api/Generated/Models/Common/PagedQueryResponse.cs
@@ -8,11 +8,11 @@ namespace commercetools.Sdk.Api.Models.Common
     {
         public long Limit { get; set; }
 
+        public long Offset { get; set; }
+
         public long Count { get; set; }
 
         public long? Total { get; set; }
-
-        public long Offset { get; set; }
 
         public List<IBaseResource> Results { get; set; }
 
