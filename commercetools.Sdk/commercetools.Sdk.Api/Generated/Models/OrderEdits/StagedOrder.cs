97ef3935caf322ffc672f7b4373f09fa4581ba83
Auto Mation <automation@commercetools.com>
2022-05-24 14:40:18 +0000

    TASK: Updating SDK

--- commercetools.Sdk/commercetools.Sdk.Api/Generated/Models/OrderEdits/StagedOrder.cs
+++ commercetools.Sdk/commercetools.Sdk.Api/Generated/Models/OrderEdits/StagedOrder.cs
@@ -74,7 +74,8 @@ namespace commercetools.Sdk.Api.Models.OrderEdits
 
         public List<IDiscountCodeInfo> DiscountCodes { get; set; }
 
-        public long LastMessageSequenceNumber { get; set; }
+        [ObsoleteAttribute("This property is obsolete", false)]
+        public long? LastMessageSequenceNumber { get; set; }
 
         public ICartReference Cart { get; set; }
 
