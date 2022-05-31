9801a60f8683aa42dcb5389db4c021aed5cad324
Auto Mation <automation@commercetools.com>
2022-05-31 13:40:35 +0000

    TASK: Updating SDK

--- commercetools.Sdk/commercetools.Sdk.Api/Generated/Models/Inventories/InventoryPagedQueryResponse.cs
+++ commercetools.Sdk/commercetools.Sdk.Api/Generated/Models/Inventories/InventoryPagedQueryResponse.cs
@@ -7,11 +7,11 @@ namespace commercetools.Sdk.Api.Models.Inventories
     {
         public long Limit { get; set; }
 
+        public long Offset { get; set; }
+
         public long Count { get; set; }
 
         public long? Total { get; set; }
-
-        public long Offset { get; set; }
 
         public List<IInventoryEntry> Results { get; set; }
     }
