6daf1f4b988faf4ed68298776d9d7cad114a1d09
Auto Mation <automation@commercetools.com>
2022-06-20 05:59:08 +0000

    TASK: Updating SDK

--- commercetools.Sdk/commercetools.Sdk.Api/Generated/Models/ProductTypes/AttributeReferenceType.cs
+++ commercetools.Sdk/commercetools.Sdk.Api/Generated/Models/ProductTypes/AttributeReferenceType.cs
@@ -1,13 +1,10 @@
-using commercetools.Sdk.Api.Models.Common;
-
-
 namespace commercetools.Sdk.Api.Models.ProductTypes
 {
     public partial class AttributeReferenceType : IAttributeReferenceType
     {
         public string Name { get; set; }
 
-        public IReferenceTypeId ReferenceTypeId { get; set; }
+        public IAttributeReferenceTypeId ReferenceTypeId { get; set; }
         public AttributeReferenceType()
         {
             this.Name = "reference";
--- commercetools.Sdk/commercetools.Sdk.Api/Generated/Models/ProductTypes/IProductTypePagedQueryResponse.cs
+++ commercetools.Sdk/commercetools.Sdk.Api/Generated/Models/ProductTypes/IProductTypePagedQueryResponse.cs
@@ -9,11 +9,11 @@ namespace commercetools.Sdk.Api.Models.ProductTypes
     {
         long Limit { get; set; }
 
+        long Offset { get; set; }
+
         long Count { get; set; }
 
         long? Total { get; set; }
-
-        long Offset { get; set; }
 
         List<IProductType> Results { get; set; }
     }
