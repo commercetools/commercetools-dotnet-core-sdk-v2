602cd63350be9b4d58752f5d71b4b1c76bca1b98
Auto Mation <automation@commercetools.com>
2022-12-06 11:35:21 +0000

    Update docs

--- docs/html/search/classes_13.js
+++ docs/html/search/classes_13.js
@@ -1,13 +1,13 @@
 var searchData=
 [
-  ['unauthorizedexception_17495',['UnauthorizedException',['../da/d18/classcommercetools_1_1_base_1_1_client_1_1_error_1_1_unauthorized_exception.html',1,'commercetools::Base::Client::Error']]],
-  ['unknownchange_17496',['UnknownChange',['../d9/d1c/classcommercetools_1_1_sdk_1_1_history_api_1_1_models_1_1_changes_1_1_unknown_change.html',1,'commercetools::Sdk::HistoryApi::Models::Changes']]],
-  ['unpublishchange_17497',['UnpublishChange',['../d5/d98/classcommercetools_1_1_sdk_1_1_history_api_1_1_models_1_1_changes_1_1_unpublish_change.html',1,'commercetools::Sdk::HistoryApi::Models::Changes']]],
-  ['unresolvedreferences_17498',['UnresolvedReferences',['../dc/dc4/classcommercetools_1_1_sdk_1_1_import_api_1_1_models_1_1_common_1_1_unresolved_references.html',1,'commercetools::Sdk::ImportApi::Models::Common']]],
-  ['update_17499',['Update',['../d1/d2d/classcommercetools_1_1_sdk_1_1_api_1_1_models_1_1_common_1_1_update.html',1,'commercetools::Sdk::Api::Models::Common']]],
-  ['updateaction_17500',['UpdateAction',['../d7/db6/classcommercetools_1_1_sdk_1_1_api_1_1_models_1_1_common_1_1_update_action.html',1,'commercetools::Sdk::Api::Models::Common']]],
-  ['updatesyncinfochange_17501',['UpdateSyncInfoChange',['../d7/dd4/classcommercetools_1_1_sdk_1_1_history_api_1_1_models_1_1_changes_1_1_update_sync_info_change.html',1,'commercetools::Sdk::HistoryApi::Models::Changes']]],
-  ['updatetypewrapper_17502',['UpdateTypeWrapper',['../d6/d82/classcommercetools_1_1_sdk_1_1_history_api_1_1_models_1_1_change_histories_1_1_update_type_wrapper.html',1,'commercetools::Sdk::HistoryApi::Models::ChangeHistories']]],
-  ['useragentprovider_17503',['UserAgentProvider',['../d6/dfe/classcommercetools_1_1_base_1_1_client_1_1_middlewares_1_1_user_agent_provider.html',1,'commercetools::Base::Client::Middlewares']]],
-  ['userprovidedidentifiers_17504',['UserProvidedIdentifiers',['../de/d2b/classcommercetools_1_1_sdk_1_1_api_1_1_models_1_1_messages_1_1_user_provided_identifiers.html',1,'commercetools::Sdk::Api::Models::Messages']]]
+  ['unauthorizedexception_17758',['UnauthorizedException',['../da/d18/classcommercetools_1_1_base_1_1_client_1_1_error_1_1_unauthorized_exception.html',1,'commercetools::Base::Client::Error']]],
+  ['unknownchange_17759',['UnknownChange',['../d9/d1c/classcommercetools_1_1_sdk_1_1_history_api_1_1_models_1_1_changes_1_1_unknown_change.html',1,'commercetools::Sdk::HistoryApi::Models::Changes']]],
+  ['unpublishchange_17760',['UnpublishChange',['../d5/d98/classcommercetools_1_1_sdk_1_1_history_api_1_1_models_1_1_changes_1_1_unpublish_change.html',1,'commercetools::Sdk::HistoryApi::Models::Changes']]],
+  ['unresolvedreferences_17761',['UnresolvedReferences',['../dc/dc4/classcommercetools_1_1_sdk_1_1_import_api_1_1_models_1_1_common_1_1_unresolved_references.html',1,'commercetools::Sdk::ImportApi::Models::Common']]],
+  ['update_17762',['Update',['../d1/d2d/classcommercetools_1_1_sdk_1_1_api_1_1_models_1_1_common_1_1_update.html',1,'commercetools::Sdk::Api::Models::Common']]],
+  ['updateaction_17763',['UpdateAction',['../d7/db6/classcommercetools_1_1_sdk_1_1_api_1_1_models_1_1_common_1_1_update_action.html',1,'commercetools::Sdk::Api::Models::Common']]],
+  ['updatesyncinfochange_17764',['UpdateSyncInfoChange',['../d7/dd4/classcommercetools_1_1_sdk_1_1_history_api_1_1_models_1_1_changes_1_1_update_sync_info_change.html',1,'commercetools::Sdk::HistoryApi::Models::Changes']]],
+  ['updatetypewrapper_17765',['UpdateTypeWrapper',['../d6/d82/classcommercetools_1_1_sdk_1_1_history_api_1_1_models_1_1_change_histories_1_1_update_type_wrapper.html',1,'commercetools::Sdk::HistoryApi::Models::ChangeHistories']]],
+  ['useragentprovider_17766',['UserAgentProvider',['../d6/dfe/classcommercetools_1_1_base_1_1_client_1_1_middlewares_1_1_user_agent_provider.html',1,'commercetools::Base::Client::Middlewares']]],
+  ['userprovidedidentifiers_17767',['UserProvidedIdentifiers',['../de/d2b/classcommercetools_1_1_sdk_1_1_api_1_1_models_1_1_messages_1_1_user_provided_identifiers.html',1,'commercetools::Sdk::Api::Models::Messages']]]
 ];
--- docs/html/search/enums_4.js
+++ docs/html/search/enums_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['extensionaction_21391',['ExtensionAction',['../d3/da4/namespacecommercetools_1_1_sdk_1_1_api_1_1_models_1_1_extensions.html#aa285fd24e5493f3dc2c796dc729584d5',1,'commercetools::Sdk::Api::Models::Extensions']]],
-  ['extensionresourcetypeid_21392',['ExtensionResourceTypeId',['../d3/da4/namespacecommercetools_1_1_sdk_1_1_api_1_1_models_1_1_extensions.html#a4a0ba23c79ec7458e4f5709a890b7883',1,'commercetools::Sdk::Api::Models::Extensions']]]
+  ['extensionaction_21692',['ExtensionAction',['../d3/da4/namespacecommercetools_1_1_sdk_1_1_api_1_1_models_1_1_extensions.html#aa285fd24e5493f3dc2c796dc729584d5',1,'commercetools::Sdk::Api::Models::Extensions']]],
+  ['extensionresourcetypeid_21693',['ExtensionResourceTypeId',['../d3/da4/namespacecommercetools_1_1_sdk_1_1_api_1_1_models_1_1_extensions.html#a4a0ba23c79ec7458e4f5709a890b7883',1,'commercetools::Sdk::Api::Models::Extensions']]]
 ];
