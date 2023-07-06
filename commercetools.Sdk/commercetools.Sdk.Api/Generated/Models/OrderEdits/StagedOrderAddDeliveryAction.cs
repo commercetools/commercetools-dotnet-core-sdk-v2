8fef70137d8c8f04bb878e8b5382a5036a7a4089
Auto Mation <automation@commercetools.com>
2023-07-06 05:18:40 +0000

    TASK: Updating SDK

--- commercetools.Sdk/commercetools.Sdk.Api/Generated/Models/OrderEdits/StagedOrderAddDeliveryAction.cs
+++ commercetools.Sdk/commercetools.Sdk.Api/Generated/Models/OrderEdits/StagedOrderAddDeliveryAction.cs
@@ -14,6 +14,8 @@ namespace commercetools.Sdk.Api.Models.OrderEdits
 
         public string DeliveryKey { get; set; }
 
+        public string ShippingKey { get; set; }
+
         public IList<IDeliveryItem> Items { get; set; }
         public IEnumerable<IDeliveryItem> ItemsEnumerable { set => Items = value.ToList(); }
 
