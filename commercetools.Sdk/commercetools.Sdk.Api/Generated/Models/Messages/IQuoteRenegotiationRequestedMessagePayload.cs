bcff5bf143273a2657f6fb3cc5bd622c39f9f07a
Auto Mation <automation@commercetools.com>
2023-08-24 14:32:45 +0000

    TASK: Updating SDK

---
+++ commercetools.Sdk/commercetools.Sdk.Api/Generated/Models/Messages/IQuoteRenegotiationRequestedMessagePayload.cs
@@ -0,0 +1,12 @@
+using commercetools.Base.CustomAttributes;
+
+// ReSharper disable CheckNamespace
+namespace commercetools.Sdk.Api.Models.Messages
+{
+    [DeserializeAs(typeof(commercetools.Sdk.Api.Models.Messages.QuoteRenegotiationRequestedMessagePayload))]
+    public partial interface IQuoteRenegotiationRequestedMessagePayload : IMessagePayload
+    {
+        string BuyerComment { get; set; }
+
+    }
+}
