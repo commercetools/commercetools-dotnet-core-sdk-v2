cc4c4608df879de82121c1769971aa9bf454c377
Jens Schulze <jens.schulze@commercetools.com>
2023-08-25 09:30:57 +0200

    add OpenTelemetry support to Example application

---
+++ commercetools.Sdk/Examples/commercetools.Api.CheckoutApp/Instrumentation.cs
@@ -0,0 +1,51 @@
+using System;
+
+// <copyright file="Instrumentation.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+namespace commercetools.Api.CheckoutApp
+{
+    using System.Diagnostics;
+    using System.Diagnostics.Metrics;
+
+    /// <summary>
+    /// It is recommended to use a custom type to hold references for
+    /// ActivitySource and Instruments. This avoids possible type collisions
+    /// with other components in the DI container.
+    /// </summary>
+    public class Instrumentation : IDisposable
+    {
+        internal const string ActivitySourceName = "Commercetools.CheckoutApp";
+        internal const string MeterName = "Commercetools.CheckoutApp";
+        private readonly Meter meter;
+
+        public Instrumentation()
+        {
+            string? version = typeof(Instrumentation).Assembly.GetName().Version?.ToString();
+            this.ActivitySource = new ActivitySource(ActivitySourceName, version);
+            this.meter = new Meter(MeterName, version);
+        }
+
+        public ActivitySource ActivitySource { get; }
+        
+        public void Dispose()
+        {
+            this.ActivitySource.Dispose();
+            this.meter.Dispose();
+        }
+    }
+}
+
