f24c214af8cf0357f0aae348bc591460d723c650
Jens Schulze <jens.schulze@commercetools.com>
2024-01-12 11:48:35 +0100

    add option to register custom loggerhandler (#256)

---
+++ commercetools.Sdk/commercetools.Base.Client/LoggerHandlerFactory.cs
@@ -0,0 +1,20 @@
+using System.Net.Http;
+using Microsoft.Extensions.Logging;
+
+namespace commercetools.Base.Client
+{
+    public class LoggerHandlerFactory: ILoggerHandlerFactory
+    {
+        private readonly ILoggerFactory loggerFactory;
+
+        public LoggerHandlerFactory(ILoggerFactory loggerFactory)
+        {
+            this.loggerFactory = loggerFactory;
+        }
+
+        public DelegatingHandler Create()
+        {
+            return new LoggerHandler(loggerFactory);
+        }
+    }
+}
