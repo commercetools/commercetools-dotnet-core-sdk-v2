34432dea946e7cf12d4123be78bce2a6fd06718c
Jens Schulze <jens.schulze@commercetools.com>
2024-06-27 11:12:26 +0200

    add datadog example app (#324)

---
+++ commercetools.Sdk/Examples/commercetools.Api.ApmExample/Models/SignInResult.cs
@@ -0,0 +1,13 @@
+using commercetools.Sdk.Api.Models.Carts;
+using commercetools.Sdk.Api.Models.Customers;
+
+namespace commercetools.Api.ApmExample.Models
+{
+    public class SignInResult : ICustomerSignInResult
+    {
+        public ICustomer Customer { get; set; }
+        public ICart Cart { get; set; }
+        public string ErrorMessage { get; set; }
+        public bool IsValidCredentials { get; set; }
+    }
+}
--- commercetools.Sdk/Examples/commercetools.Api.DynatraceExample/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace commercetools.Api.DynatraceExample.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
