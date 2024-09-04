4232d32d3d5488f0bac0647fb82c88fa72877ff3
Auto Mation <automation@commercetools.com>
2024-09-04 07:35:44 +0000

    Update docs

--- docs/html/search/classes_14.js
+++ docs/html/search/classes_14.js
@@ -4,5 +4,6 @@ var searchData=
   ['variablequerypredicate_1',['VariableQueryPredicate',['../de/dc8/classcommercetools_1_1_sdk_1_1_api_1_1_predicates_1_1_query_1_1_variable_query_predicate.html',1,'commercetools::Sdk::Api::Predicates::Query']]],
   ['variant_2',['Variant',['../dd/d6e/classcommercetools_1_1_sdk_1_1_history_api_1_1_models_1_1_common_1_1_variant.html',1,'commercetools::Sdk::HistoryApi::Models::Common']]],
   ['variantvalues_3',['variantvalues',['../dc/dce/classcommercetools_1_1_sdk_1_1_api_1_1_models_1_1_errors_1_1_variant_values.html',1,'commercetools.Sdk.Api.Models.Errors.VariantValues'],['../de/dd2/classcommercetools_1_1_sdk_1_1_import_api_1_1_models_1_1_errors_1_1_variant_values.html',1,'commercetools.Sdk.ImportApi.Models.Errors.VariantValues']]],
-  ['verifyemailchange_4',['VerifyEmailChange',['../da/d1c/classcommercetools_1_1_sdk_1_1_history_api_1_1_models_1_1_changes_1_1_verify_email_change.html',1,'commercetools::Sdk::HistoryApi::Models::Changes']]]
+  ['verifyemailchange_4',['VerifyEmailChange',['../da/d1c/classcommercetools_1_1_sdk_1_1_history_api_1_1_models_1_1_changes_1_1_verify_email_change.html',1,'commercetools::Sdk::HistoryApi::Models::Changes']]],
+  ['versionmiddleware_5',['VersionMiddleware',['../d4/df0/classcommercetools_1_1_base_1_1_client_1_1_middlewares_1_1_version_middleware.html',1,'commercetools::Base::Client::Middlewares']]]
 ];
