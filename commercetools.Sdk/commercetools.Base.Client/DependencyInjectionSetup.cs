c17caa6759448ec65ad50e219bcc75e23e7451ad
Jens Schulze <jens.schulze@commercetools.com>
2024-10-07 14:10:19 +0200

    refactor loggerhandler (#273)
    * refactor loggerhandler
    
    * support multipe loglevel for exceptions
    
    * better logger format in CI
    
    * add test for logging
    
    * add test for default logging output

--- commercetools.Sdk/commercetools.Base.Client/DependencyInjectionSetup.cs
+++ commercetools.Sdk/commercetools.Base.Client/DependencyInjectionSetup.cs
@@ -105,6 +105,8 @@ namespace commercetools.Base.Client
             options ??= new ClientOptions();
             services.AddSingleton<IUserAgentProvider, UserAgentProvider>();
             services.AddSingleton<ILoggerHandlerFactory, LoggerHandlerFactory>();
+            services.AddSingleton<IHttpLogger, DefaultHttpLogger>();
+            services.AddSingleton<ILoggerHandlerOptions, LoggerHandlerOptions>();
             var httpClientBuilder = services.AddHttpClient(clientName)
                 .ConfigureHttpClient((provider, client) =>
                 {
@@ -112,10 +114,12 @@ namespace commercetools.Base.Client
                     {
                         client.DefaultRequestHeaders.AcceptEncoding.ParseAdd("gzip");
                     }
+
                     if (options.DecompressionMethods.HasFlag(DecompressionMethods.Deflate))
                     {
                         client.DefaultRequestHeaders.AcceptEncoding.ParseAdd("deflate");
                     }
+
                     var userAgentProvider = provider.GetService<IUserAgentProvider>() ?? new UserAgentProvider();
                     client.DefaultRequestHeaders.UserAgent.ParseAdd(userAgentProvider.UserAgent);
                 })
@@ -126,8 +130,9 @@ namespace commercetools.Base.Client
                         AutomaticDecompression = options.DecompressionMethods
                     };
                 })
-                .AddHttpMessageHandler(c => new ErrorHandler(message => serializerFactory(c).Deserialize(errorResponseTypeMapper(message), message.ExtractResponseBody())))
-                .AddHttpMessageHandler(c => c.GetService<ILoggerHandlerFactory>().Create());
+                .AddHttpMessageHandler(c => c.GetService<ILoggerHandlerFactory>().Create())
+                .AddHttpMessageHandler(c => new ErrorHandler(message =>
+                    serializerFactory(c).Deserialize(errorResponseTypeMapper(message), message.ExtractResponseBody())));
 
             return httpClientBuilder;
         }
