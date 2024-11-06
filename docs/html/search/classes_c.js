51cd8fddb7af00d4026146850e167f203575dae6
Auto Mation <automation@commercetools.com>
2024-11-06 15:59:05 +0000

    Update docs

--- docs/html/search/classes_c.js
+++ docs/html/search/classes_c.js
@@ -1,10 +1,11 @@
 var searchData=
 [
   ['nestedattribute_0',['NestedAttribute',['../df/df0/classcommercetools_1_1_sdk_1_1_api_1_1_models_1_1_products_1_1_nested_attribute.html',1,'commercetools::Sdk::Api::Models::Products']]],
-  ['nomatchingproductdiscountfounderror_1',['NoMatchingProductDiscountFoundError',['../d2/d25/classcommercetools_1_1_sdk_1_1_api_1_1_models_1_1_errors_1_1_no_matching_product_discount_found_error.html',1,'commercetools::Sdk::Api::Models::Errors']]],
-  ['notfoundexception_2',['NotFoundException',['../d3/db4/classcommercetools_1_1_base_1_1_client_1_1_error_1_1_not_found_exception.html',1,'commercetools::Base::Client::Error']]],
-  ['numberattribute_3',['NumberAttribute',['../d2/d3c/classcommercetools_1_1_sdk_1_1_import_api_1_1_models_1_1_productvariants_1_1_number_attribute.html',1,'commercetools::Sdk::ImportApi::Models::Productvariants']]],
-  ['numberfield_4',['NumberField',['../d4/db7/classcommercetools_1_1_sdk_1_1_import_api_1_1_models_1_1_customfields_1_1_number_field.html',1,'commercetools::Sdk::ImportApi::Models::Customfields']]],
-  ['numbersetattribute_5',['NumberSetAttribute',['../d9/d34/classcommercetools_1_1_sdk_1_1_import_api_1_1_models_1_1_productvariants_1_1_number_set_attribute.html',1,'commercetools::Sdk::ImportApi::Models::Productvariants']]],
-  ['numbersetfield_6',['NumberSetField',['../dd/d39/classcommercetools_1_1_sdk_1_1_import_api_1_1_models_1_1_customfields_1_1_number_set_field.html',1,'commercetools::Sdk::ImportApi::Models::Customfields']]]
+  ['newmastervariantadditionnotallowederror_1',['NewMasterVariantAdditionNotAllowedError',['../db/df6/classcommercetools_1_1_sdk_1_1_import_api_1_1_models_1_1_errors_1_1_new_master_variant_addition_not_allowed_error.html',1,'commercetools::Sdk::ImportApi::Models::Errors']]],
+  ['nomatchingproductdiscountfounderror_2',['NoMatchingProductDiscountFoundError',['../d2/d25/classcommercetools_1_1_sdk_1_1_api_1_1_models_1_1_errors_1_1_no_matching_product_discount_found_error.html',1,'commercetools::Sdk::Api::Models::Errors']]],
+  ['notfoundexception_3',['NotFoundException',['../d3/db4/classcommercetools_1_1_base_1_1_client_1_1_error_1_1_not_found_exception.html',1,'commercetools::Base::Client::Error']]],
+  ['numberattribute_4',['NumberAttribute',['../d2/d3c/classcommercetools_1_1_sdk_1_1_import_api_1_1_models_1_1_productvariants_1_1_number_attribute.html',1,'commercetools::Sdk::ImportApi::Models::Productvariants']]],
+  ['numberfield_5',['NumberField',['../d4/db7/classcommercetools_1_1_sdk_1_1_import_api_1_1_models_1_1_customfields_1_1_number_field.html',1,'commercetools::Sdk::ImportApi::Models::Customfields']]],
+  ['numbersetattribute_6',['NumberSetAttribute',['../d9/d34/classcommercetools_1_1_sdk_1_1_import_api_1_1_models_1_1_productvariants_1_1_number_set_attribute.html',1,'commercetools::Sdk::ImportApi::Models::Productvariants']]],
+  ['numbersetfield_7',['NumberSetField',['../dd/d39/classcommercetools_1_1_sdk_1_1_import_api_1_1_models_1_1_customfields_1_1_number_set_field.html',1,'commercetools::Sdk::ImportApi::Models::Customfields']]]
 ];
