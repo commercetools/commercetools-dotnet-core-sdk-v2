939733244e3b2c9ad2eb8868c43b2d4039557832
sommmen <Dion_nl@yahoo.co.uk>
2025-02-04 10:24:40 +0100

    feat(api): Option to not fail on head calls (#396)
    * poc done
    
    * rem old code
    
    * refactor to make the check configurable
    
    * remove unused imports
    
    ---------
    
    Co-authored-by: DioLeu <dion@onlineplasticsgroup.com>
    Co-authored-by: Jens Schulze <jens.schulze@commercetools.com>

--- commercetools.Sdk/commercetools.Base.Client/ErrorHandler.cs
+++ commercetools.Sdk/commercetools.Base.Client/ErrorHandler.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,22 +10,30 @@ namespace commercetools.Base.Client
     {
         private readonly Func<HttpResponseMessage, object> _errorResponseBodyMapper;
 
-        public ErrorHandler(Func<HttpResponseMessage, object> errorResponseBodyMapper)
+        private readonly bool _notFoundReturnsEmpty;
+        private readonly bool _headNotFoundReturnsEmpty;
+
+        public ErrorHandler(Func<HttpResponseMessage, object> errorResponseBodyMapper, bool notFoundReturnsEmpty = false, bool headNotFoundReturnsEmpty = false)
         {
             this._errorResponseBodyMapper = errorResponseBodyMapper;
+            this._notFoundReturnsEmpty = notFoundReturnsEmpty;
+            this._headNotFoundReturnsEmpty = headNotFoundReturnsEmpty;
         }
 
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
             var response = await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
 
-            if (response is { IsSuccessStatusCode: false })
+            if (response is { IsSuccessStatusCode: true })
+                return response;
+
+            if (response.StatusCode == HttpStatusCode.NotFound && (_notFoundReturnsEmpty || (_headNotFoundReturnsEmpty && request.Method == HttpMethod.Head) ))
             {
-                var exception = ExceptionFactory.Create(request, response, _errorResponseBodyMapper);
-                throw exception;
+                response.Content = new EmptyContent();
+                return response;
             }
 
-            return response;
+            throw ExceptionFactory.Create(request, response, _errorResponseBodyMapper);
         }
     }
 }
