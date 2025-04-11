42b52e94b24178f20c1374f0bbca6bb3e6a20a48
Jens Schulze <jens.schulze@commercetools.com>
2025-04-11 23:16:02 +0200

    add instore query methods

---
+++ commercetools.Sdk/commercetools.Sdk.Api/Client/RequestBuilders/InStore/ByProjectKeyInStoreKeyByStoreKeyCartDiscountsGet.cs
@@ -0,0 +1,9 @@
+using commercetools.Sdk.Api.Models.CartDiscounts;
+using commercetools.Sdk.Api.Predicates.Query.CartDiscounts;
+
+namespace commercetools.Sdk.Api.Client.RequestBuilders.InStore;
+
+public partial class ByProjectKeyInStoreKeyByStoreKeyCartDiscountsGet : IPagedQueryResourceRequest<ByProjectKeyInStoreKeyByStoreKeyCartDiscountsGet, ICartDiscountPagedQueryResponse, CartDiscountQueryBuilderDsl>, commercetools.Sdk.Api.Models.IPagedQueryResourceRequest<ByProjectKeyInStoreKeyByStoreKeyCartDiscountsGet,
+    commercetools.Sdk.Api.Models.CartDiscounts.ICartDiscountPagedQueryResponse>
+{
+}
---
+++ commercetools.Sdk/commercetools.Sdk.Api/Client/RequestBuilders/InStore/ByProjectKeyInStoreKeyByStoreKeyQuoteRequestsGet.cs
@@ -0,0 +1,9 @@
+using commercetools.Sdk.Api.Models.QuoteRequests;
+using commercetools.Sdk.Api.Predicates.Query.QuoteRequests;
+
+namespace commercetools.Sdk.Api.Client.RequestBuilders.InStore;
+
+public partial class ByProjectKeyInStoreKeyByStoreKeyQuoteRequestsGet : IPagedQueryResourceRequest<ByProjectKeyInStoreKeyByStoreKeyQuoteRequestsGet, IQuoteRequestPagedQueryResponse, QuoteRequestQueryBuilderDsl>, commercetools.Sdk.Api.Models.IPagedQueryResourceRequest<ByProjectKeyInStoreKeyByStoreKeyQuoteRequestsGet,
+    commercetools.Sdk.Api.Models.QuoteRequests.IQuoteRequestPagedQueryResponse>
+{
+}
