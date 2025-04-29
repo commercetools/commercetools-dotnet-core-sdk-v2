f8802ebb5048f6be8c2dc862381698c0f7b52f27
Auto Mation <automation@commercetools.com>
2025-04-29 08:52:16 +0000

    Update docs

--- docs/html/search/enums_4.js
+++ docs/html/search/enums_4.js
@@ -1,5 +1,7 @@
 var searchData=
 [
-  ['extensionaction_0',['ExtensionAction',['../d3/da4/namespacecommercetools_1_1_sdk_1_1_api_1_1_models_1_1_extensions.html#aa285fd24e5493f3dc2c796dc729584d5',1,'commercetools::Sdk::Api::Models::Extensions']]],
-  ['extensionresourcetypeid_1',['ExtensionResourceTypeId',['../d3/da4/namespacecommercetools_1_1_sdk_1_1_api_1_1_models_1_1_extensions.html#a4a0ba23c79ec7458e4f5709a890b7883',1,'commercetools::Sdk::Api::Models::Extensions']]]
+  ['eventsubscriptionresourcetypeid_0',['EventSubscriptionResourceTypeId',['../d9/dc1/namespacecommercetools_1_1_sdk_1_1_api_1_1_models_1_1_subscriptions.html#a6c60fae478dfd3fcbe7e1471e014f4f9',1,'commercetools::Sdk::Api::Models::Subscriptions']]],
+  ['eventtype_1',['EventType',['../d9/dc1/namespacecommercetools_1_1_sdk_1_1_api_1_1_models_1_1_subscriptions.html#a7058a33f471593dbd022674d876e33e7',1,'commercetools::Sdk::Api::Models::Subscriptions']]],
+  ['extensionaction_2',['ExtensionAction',['../d3/da4/namespacecommercetools_1_1_sdk_1_1_api_1_1_models_1_1_extensions.html#aa285fd24e5493f3dc2c796dc729584d5',1,'commercetools::Sdk::Api::Models::Extensions']]],
+  ['extensionresourcetypeid_3',['ExtensionResourceTypeId',['../d3/da4/namespacecommercetools_1_1_sdk_1_1_api_1_1_models_1_1_extensions.html#a4a0ba23c79ec7458e4f5709a890b7883',1,'commercetools::Sdk::Api::Models::Extensions']]]
 ];
