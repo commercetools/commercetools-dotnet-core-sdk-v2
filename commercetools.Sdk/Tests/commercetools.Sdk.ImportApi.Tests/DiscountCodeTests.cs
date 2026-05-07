caf0e1963fc3080da182fe092696c0b95d8d693d
Jens Schulze <jens.schulze@commercetools.com>
2026-05-07 10:01:11 +0200

    add test for serialization of datetimes

---
+++ commercetools.Sdk/Tests/commercetools.Sdk.ImportApi.Tests/DiscountCodeTests.cs
@@ -0,0 +1,32 @@
+using System;
+using commercetools.Base.Models;
+using commercetools.Sdk.ImportApi.Models.Common;
+using commercetools.Sdk.ImportApi.Models.Customers;
+using commercetools.Sdk.ImportApi.Models.DiscountCodes;
+using commercetools.Sdk.ImportApi.Serialization;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace commercetools.Sdk.ImportApi.Tests;
+
+public class DiscountCodeTests
+{
+    [Fact]
+    public void Test_Serialize_Date()
+    {
+        var s = new ServiceCollection();
+        s.UseCommercetoolsImportApiSerialization();
+
+        var p = s.BuildServiceProvider();
+        var serializerService = p.GetService<IImportSerializerService>();
+
+        var discountCodeImport = new DiscountCodeImport
+        {
+            ValidFrom = DateTime.Parse("2026-01-01T12:00:00+01:00"),
+            ValidUntil = DateTime.Parse("2026-01-31T12:00:00+01:00"),
+        };
+        var t = serializerService.Serialize(discountCodeImport);
+
+        Assert.Equal("{\"isActive\":false,\"validFrom\":\"2026-01-01T11:00:00Z\",\"validUntil\":\"2026-01-31T11:00:00Z\"}", t);
+    }
+}
