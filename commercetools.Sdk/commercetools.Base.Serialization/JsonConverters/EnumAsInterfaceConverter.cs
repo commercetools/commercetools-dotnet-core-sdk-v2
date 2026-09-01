c94f32dbe7ae151d4d8158471e84c00ec5ac8ceb
Jens Schulze <jens.schulze@commercetools.com>
2026-09-01 14:06:25 +0200

    reduce dynamic instantiation

--- commercetools.Sdk/commercetools.Base.Serialization/JsonConverters/EnumAsInterfaceConverter.cs
+++ commercetools.Sdk/commercetools.Base.Serialization/JsonConverters/EnumAsInterfaceConverter.cs
@@ -14,7 +14,7 @@ namespace commercetools.Base.Serialization.JsonConverters
 
         protected JsonSerializerOptions JsonSerializerOptions { get; }
 
-        protected EnumInterfaceCreatorAttribute EnumInterfaceCreator { get; }
+        protected Delegate EnumInterfaceCreator { get; }
 
         public EnumAsInterfaceConverter(JsonNamingPolicy namingPolicy, JsonSerializerOptions jsonSerializerOptions)
         {
@@ -22,13 +22,12 @@ namespace commercetools.Base.Serialization.JsonConverters
             this.JsonSerializerOptions = jsonSerializerOptions;
 
             var enumInterfaceCreatorAttribute = typeof(T).GetCustomAttribute<EnumInterfaceCreatorAttribute>();
-            EnumInterfaceCreator = enumInterfaceCreatorAttribute ?? throw new NullReferenceException($"Failed to find the required '{nameof(EnumInterfaceCreatorAttribute)}'");
+            EnumInterfaceCreator = enumInterfaceCreatorAttribute?.Creator() ?? throw new NullReferenceException($"Failed to find the required '{nameof(EnumInterfaceCreatorAttribute)}'");
         }
 
         public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            var attrDelegate = EnumInterfaceCreator.Creator();
-            return (T)attrDelegate.DynamicInvoke(reader.GetString());
+            return (T)EnumInterfaceCreator.DynamicInvoke(reader.GetString());
         }
 
         public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
